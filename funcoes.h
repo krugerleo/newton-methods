fcb760ce7968438bc79cdda1e1aa3c193252cd8e
krugerleo <kruger.leob@gmail.com>
2022-04-01 15:52:57 -0300

    Add ifndef

--- funcoes.h
+++ funcoes.h
@@ -1,3 +1,5 @@
+#ifndef UTILS_H
+#define UTILS_H
 SistemaL_Convertido *alocaSistemaLConvertido(unsigned int n);
 
 SistemaL_Convertido *converteMatriz(Sistema_Linear *SL);
@@ -5,3 +7,4 @@ SistemaL_Convertido *converteMatriz(Sistema_Linear *SL);
 Sistema_Linear *lerSistema();
 
 Sistema_Linear *alocaSistemaLinear(unsigned int n);
+#endif
